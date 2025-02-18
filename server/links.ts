a19fd21f50cd0c8303589867bb5119fafa203e12
harryadel <harryadelb@gmail.com>
2025-02-18 12:55:08 +0200

    Get autoremoval to work

--- server/links.ts
+++ server/links.ts
@@ -2,18 +2,6 @@ import { Meteor } from "meteor/meteor";
 import { Products } from "/imports/api/collections/products";
 import { Categories } from "/imports/api/collections/categories";
 
- Products.collection.addLinks({
-   user: {
-     collection: Meteor.users,
-     field: "userId",
-     type: "one",
-   },
-   categories: {
-     collection: Categories.collection,
-     field: "categoryIds",
-     type: "many"
-   },
- });
 
 Categories.collection.addLinks({
   user: {
@@ -23,7 +11,21 @@ Categories.collection.addLinks({
   },
   products: {
     collection: Products.collection,
-    autoremove: true,
-    // inversedBy: "categories",
+    inversedBy: "categories"
   }
 });
+
+Products.collection.addLinks({
+   user: {
+     collection: Meteor.users,
+     field: "userId",
+     type: "one",
+   },
+   categories: {
+     collection: Categories.collection,
+     field: "categoryIds",
+     type: "many",
+     autoremove: true
+   },
+ });
+
